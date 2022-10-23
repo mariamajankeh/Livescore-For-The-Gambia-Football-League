59a55551295f104fbc78a0c38e0beb59723a395d
mariamajankeh <jankehsecka22@gmail.com>
2022-10-23 16:59:19 +0100

    commit request

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -14,67 +14,71 @@ const User = require('../models/user');
   }
  };
 
-
-
  const getAllUsers = async (req, res) => {
-    try {
-      const users = await User.find({});
-      return res.status(201).json({ data: users });
-   } catch (error) {
-      console.log(error);
-      return res.status(500).end();
-   }
+   try {
+     const users = await User.find({});
+     return res.status(201).json({ data: users });
+  } catch (error) {
+     console.log(error);
+     return res.status(500).end();
+  }
 };
 
 const getOneUser = async (req, res) => {
-    const id = req.params.id
-    try {
-      const user = await User.findOne({ _id: id });
-      if (!user) {
-        return res.status(400).json({ message: 'user not found' });
-     }
-      return res.status(201).json({ data: user });
-   } catch (error) {
-      console.log(error);
-      return res.status(500).end();
-   }
+   const id = req.params.id
+   try {
+     const user = await User.findOne({ _id: id });
+     if (!user) {
+       return res.status(400).json({ message: 'user not found' });
+    }
+     return res.status(201).json({ data: user });
+  } catch (error) {
+     console.log(error);
+     return res.status(500).end();
+
+}
 };
 
 const updateOne = async (req, res) => {
-    const id = req.params.id
-    const content = req.body
-    try {
-      const user = await User.findOneAndUpdate(
-       { _id: id },
-        content,
-       { new: true }
-     );
-      if (!user) {
-
-        return res.status(400).json({ message: 'user not found' });
+   const id = req.params.id
+   const content = req.body
+   try {
+     const user = await User.findOneAndUpdate(
+      { _id: id },
+       content,
+      { new: true }
+    );
+     if (!user) {
+      return res.status(400).json({ message: 'user not found' });
    }
     return res.status(201).json({ data: user });
  } catch (error) {
     console.log(error);
     return res.status(500).end();
- }
+
+     }
+   };
+
+   const deleteOne = async (req, res) => {
+      const id = req.params.id
+      try {
+        const user = await User.findOneAndRemove({ _id: id });
+        if (!user) {
+          return res.status(400).json({ message: 'user not found' });
+       }
+        return res.status(201).json({ message: 'deleted successfully',
+    data: user });
+     } catch (error) {
+        console.log(error);
+        return res.status(500).end();
+
+}
 };
 
-const deleteOne = async (req, res) => {
-   const id = req.params.id
-   try {
-     const user = await User.findOneAndRemove({ _id: id });
-     if (!user) {
-       return res.status(400).json({ message: 'user not found' });
-     }
-     return res.status(201).json({ message: 'deleted successfully', 
- data: user });
-   } catch (error) {
-     console.log(error);
-     return res.status(500).end();
-   }
- };
 
+
+
+ 
  
  
  
@@ -84,6 +88,10 @@ const deleteOne = async (req, res) => {
   getOneUser,
   updateOne,
   deleteOne
+
+  
+
+  
 
 
  };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,17 +1,15 @@
 const express = require('express');
- const router = express.Router();
- 
- //const { createUser } = require('../controllers/user');
- const { createUser, getAllUsers, getOneUser, updateOne, deleteOne  } = require('../controllers/user');
- 
- 
-  //User route
- router.post('/user', createUser);
- router.get('/user', getAllUsers);
- router.get('/user/:id', getOneUser);
- router.put('/user/:id', updateOne);
- router.delete('/user/:id', deleteOne);
+const router = express.Router();
 
- 
- 
- module.exports = router;
+const { createUser, getAllUsers, getOneUser, updateOne, deleteOne } = require('../controllers/user');
+
+// User route
+router.post('/user', createUser);
+router.get('/user', getAllUsers);
+router.get('/user/:id', getOneUser);
+router.put('/user/:id', updateOne);
+router.delete('/user/:id', deleteOne);
+
+
+module.exports = router;
+
